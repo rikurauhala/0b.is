4c1aa8effda6dfa6c9669a4bf08fb732860f20fb
Riku Rauhala <riku@rauhala.io>
2022-12-18 21:37:07 +0200

    Create a simple layout with Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
-const App = (): JSX.Element => {
-  return (
-    <div>
-      Hello there!
-    </div>
-  );
-};
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
+
+const App = (): JSX.Element => (
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Container maxWidth='lg'>
+      <Grid container spacing={1} justifyContent="center" alignItems="center" >
+        <Grid item>
+          <FormControl fullWidth sx={{ m: 2 }}>
+            <InputLabel>Decimal</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-decimal"
+              label="Decimal"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl disabled fullWidth sx={{ m: 2 }}>
+            <InputLabel>Binary</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              label="Binary"
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Container>
+  </ThemeProvider>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
