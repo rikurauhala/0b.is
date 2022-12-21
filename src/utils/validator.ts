9f87bee23f8a5e9c2faa9ce7807ca161db2fd3ec
Riku Rauhala <riku@rauhala.io>
2022-12-21 23:08:35 +0200

    Use Regex for validation

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,8 +1,4 @@
-export const validateBinary = (binary: string) => {
-  for (let i = 0; i < binary.length; i++) {
-    if (binary[i] !== '0' && binary[i] !== '1') {
-      return false;
-    }
-  }
-  return true;
+export const validateBinary = (binary: string): boolean => {
+  const regex = new RegExp('^[-]?[01]*$');
+  return regex.test(binary);
 };
