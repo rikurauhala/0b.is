77a7c3a9900eece337f96c0f29e92e5cef9add65
Riku Rauhala <riku@rauhala.io>
2022-12-21 15:48:15 +0200

    Create a placeholder theme for light theme

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,5 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
