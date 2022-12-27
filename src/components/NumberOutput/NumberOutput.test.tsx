87e2e0c0ebc270828d1259adc3effee86a1de09b
Riku Rauhala <riku@rauhala.io>
2022-12-27 21:20:13 +0200

    Write tests for decimal to binary conversion

--- src/components/NumberOutput/NumberOutput.test.tsx
+++ src/components/NumberOutput/NumberOutput.test.tsx
@@ -4,7 +4,8 @@ import { render } from '@testing-library/react';
 import NumberOutput from './NumberOutput';
 
 describe('<NumberOutput />', () => {
-  test('test output with different inputs', () => {
+  test('binary to decimal', () => {
+    const errorMessage = 'Not a valid binary number!';
     const inputs = {
       '0': '0',
       '1': '1',
@@ -14,14 +15,41 @@ describe('<NumberOutput />', () => {
       '-1': '-1',
       '0000010100100010011000': '84120',
       '10000000000000000000000000000000': '2147483648',
-      '2': 'Not a valid binary number!',
-      'string': 'Not a valid binary number!',
-      '123': 'Not a valid binary number!',
+      '2': errorMessage,
+      'string': errorMessage,
+      '123': errorMessage,
     };
 
     for (const [input, expectedOutput] of Object.entries(inputs)) {
       const { container } = render(
         <NumberOutput input={input} mode='BinToDec' />
+      );
+      const div = container.querySelector('.output');
+      expect(div).toBeDefined();
+      expect(div).toHaveTextContent(expectedOutput);
+    }
+  });
+
+  test('decimal to binary', () => {
+    const errorMessage = 'Not a valid decimal number!';
+    const inputs = {
+      '0': '0',
+      '1': '1',
+      '2': '10',
+      '3': '11',
+      '4': '100',
+      '5': '101',
+      '6': '110',
+      '7': '111',
+      '99': '1100011',
+      '-0': '0',
+      '-1': '-1',
+      'abc': errorMessage,
+    };
+
+    for (const [input, expectedOutput] of Object.entries(inputs)) {
+      const { container } = render(
+        <NumberOutput input={input} mode='DecToBin' />
       );
       const div = container.querySelector('.output');
       expect(div).toBeDefined();
