09ffc6668779ae624450c7dfe8fa1760799a6c30
Riku Rauhala <riku@rauhala.io>
2023-01-01 16:58:33 +0200

    Test positive and negative zero values

--- src/utils/convert.test.ts
+++ src/utils/convert.test.ts
@@ -5,13 +5,20 @@ import { NumeralSystem } from '../types/types';
 import convert from './convert';
 
 describe('convert', () => {
-  const general = (inputs: {[key: string]: string}, from: NumeralSystem, to: NumeralSystem) => {
+  const testGeneral = (inputs: {[key: string]: string}, from: NumeralSystem, to: NumeralSystem) => {
     for (const [input, output] of Object.entries(inputs)) {
       const positive = convert(input, from, to);
       expect(positive).toBe(output);
       const negative = convert('-'.concat(input), from, to);
       expect(negative).toBe('-'.concat(output));
     }
+  };
+
+  const testZero = (from: NumeralSystem, to: NumeralSystem) => {
+    const positive = convert('0', from, to);
+    expect(positive).toBe('0');
+    const negative = convert('-0', from, to);
+    expect(negative).toBe('0');
   };
 
   test('binary to decimal', () => {
@@ -28,8 +35,8 @@ describe('convert', () => {
       '0000010100100010011000': '84120',
       '10000000000000000000000000000000': '2147483648',
     };
-
-    general(inputs, NumeralSystem.Binary, NumeralSystem.Decimal);
+    testZero(NumeralSystem.Binary, NumeralSystem.Decimal);
+    testGeneral(inputs, NumeralSystem.Binary, NumeralSystem.Decimal);
   });
 
   test('decimal to binary', () => {
@@ -45,7 +52,8 @@ describe('convert', () => {
       '99': '1100011',
       '123456789': '111010110111100110100010101',
     };
-    general(inputs, NumeralSystem.Decimal, NumeralSystem.Binary);
+    testZero(NumeralSystem.Decimal, NumeralSystem.Binary);
+    testGeneral(inputs, NumeralSystem.Decimal, NumeralSystem.Binary);
   });
 
   test('decimal to hexadecimal', () => {
@@ -66,6 +74,7 @@ describe('convert', () => {
       '32': '20',
       '123456789': '75BCD15',
     };
-    general(inputs, NumeralSystem.Decimal, NumeralSystem.Hexadecimal);
+    testZero(NumeralSystem.Decimal, NumeralSystem.Hexadecimal);
+    testGeneral(inputs, NumeralSystem.Decimal, NumeralSystem.Hexadecimal);
   });
 });
