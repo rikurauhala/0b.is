f604485c6b2a17167c2080cfbb16f34bb3a51f07
Riku Rauhala <riku@rauhala.io>
2023-01-01 14:44:31 +0200

    Convert numbers to uppercase

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,7 +1,7 @@
 import { NumeralSystem } from '../types/types';
 
 const convert = (number: string, input: NumeralSystem, output: NumeralSystem): string => {
-  return parseInt(number, input).toString(output);
+  return parseInt(number, input).toString(output).toUpperCase();
 };
 
 export default convert;
