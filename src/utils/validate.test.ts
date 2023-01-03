35e549214badd99bcdfb0da9f28673d8173623c7
Riku Rauhala <riku@rauhala.io>
2023-01-03 16:10:37 +0200

    Refactor validate.test.ts to eliminate copy-paste

--- src/utils/validate.test.ts
+++ src/utils/validate.test.ts
@@ -5,112 +5,313 @@ import { NumeralSystem } from '../types/types';
 import validate from './validate';
 
 describe('validate', () => {
-  const testGeneral = (inputs: {[key: string]: boolean}, system: NumeralSystem) => {
+  const testGeneral = (system: NumeralSystem) => {
     for (const [input, expectedValidity] of Object.entries(inputs)) {
-      const validity = validate(input, system);
-      expect(validity).toBe(expectedValidity);
+      const positive = validate(input, system);
+      expect(positive).toBe(expectedValidity[system]);
+      const negative = validate(`-${input}`, system);
+      expect(negative).toBe(expectedValidity[system]);
     }
   };
 
   test('binary', () => {
-    const inputs = {
-      '0': true,
-      '1': true,
-      '01': true,
-      '10': true,
-      '-0': true,
-      '-1': true,
-      '0000010100100010011000': true,
-      '10000000000000000000000000000000': true,
-      '2': false,
-      'string': false,
-      '123': false,
-      ' ': false,
-      '1.0': false,
-      '0.1': false,
-    };
-    testGeneral(inputs, NumeralSystem.Binary);
+    testGeneral(NumeralSystem.Binary);
   });
 
   test('octal', () => {
-    const inputs = {
-      '0': true,
-      '1': true,
-      '2': true,
-      '3': true,
-      '4': true,
-      '5': true,
-      '6': true,
-      '7': true,
-      '8': false,
-      '9': false,
-      '10': true,
-      '11': true,
-      '17': true,
-      '18': false,
-      '19': false,
-      '20': true,
-      'abc': false,
-      '1a2': false,
-      '1.': false,
-      '-1.': false,
-      '-1a': false,
-      ' ': false,
-    };
-    testGeneral(inputs, NumeralSystem.Octal);
+    testGeneral(NumeralSystem.Octal);
   });
 
   test('decimal', () => {
-    const inputs = {
-      '0': true,
-      '1': true,
-      '2': true,
-      '3': true,
-      '4': true,
-      '5': true,
-      '6': true,
-      '7': true,
-      '99': true,
-      '-0': true,
-      '-1': true,
-      'abc': false,
-      '1a2': false,
-      '1.': false,
-      '-1.': false,
-      '-1a': false,
-      ' ': false,
-    };
-    testGeneral(inputs, NumeralSystem.Decimal);
+    testGeneral(NumeralSystem.Decimal);
   });
 
   test('hexadecimal', () => {
-    const inputs = {
-      '0': true,
-      '1': true,
-      '2': true,
-      '3': true,
-      '9': true,
-      'A': true,
-      'a': true,
-      'B': true,
-      'b': true,
-      'C': true,
-      'c': true,
-      'D': true,
-      'd': true,
-      'E': true,
-      'e': true,
-      'F': true,
-      'f': true,
-      '10': true,
-      '11': true,
-      'ABCDEF': true,
-      'abcdef': true,
-      'ABCDEFG': false,
-      'G': false,
-      'g': false,
-      ' ': false,
-    };
-    testGeneral(inputs, NumeralSystem.Hexadecimal);
+    testGeneral(NumeralSystem.Hexadecimal);
   });
 });
+
+const inputs = {
+  '0': {
+    '2': true,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '1': {
+    '2': true,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '2': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '3': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '4': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '5': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '6': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '7': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '8': {
+    '2': false,
+    '8': false,
+    '10': true,
+    '16': true,
+  },
+  '9': {
+    '2': false,
+    '8': false,
+    '10': true,
+    '16': true,
+  },
+  '10': {
+    '2': true,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '11': {
+    '2': true,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '12': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '13': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '14': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '15': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '16': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '17': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  '18': {
+    '2': false,
+    '8': false,
+    '10': true,
+    '16': true,
+  },
+  '19': {
+    '2': false,
+    '8': false,
+    '10': true,
+    '16': true,
+  },
+  '20': {
+    '2': false,
+    '8': true,
+    '10': true,
+    '16': true,
+  },
+  'A': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'B': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'C': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'D': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'E': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'F': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': true,
+  },
+  'G': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'H': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'I': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'J': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'K': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'L': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'M': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'N': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'O': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'P': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'Q': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'R': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'S': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'T': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'U': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'V': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'W': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'X': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'Y': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+  'Z': {
+    '2': false,
+    '8': false,
+    '10': false,
+    '16': false,
+  },
+};
