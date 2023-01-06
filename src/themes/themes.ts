020a53903a6a1c2ae89bdbfb09734ccd8a5a6324
Riku Rauhala <riku@rauhala.io>
2023-01-06 11:25:57 +0200

    Update the theme colors

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -3,11 +3,27 @@ import { createTheme } from '@mui/material/styles';
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      main: '#00796b',
+    },
   },
 });
 
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#1de9b6',
+    },
+    secondary: {
+      main: '#00bfa5',
+    },
+    background: {
+      default: '#101010',
+      paper: '#161616',
+    },
+    error: {
+      main: '#ff1744',
+    },
   },
 });
