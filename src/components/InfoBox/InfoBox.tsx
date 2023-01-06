90637536a70d28e51b85849b55830b01efae3e42
Riku Rauhala <riku@rauhala.io>
2023-01-06 14:10:08 +0200

    Add 'Read more' links to info boxes

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 
 import ReactMarkdown from 'react-markdown';
@@ -11,29 +12,47 @@ import ContentOctal from './content/octal.md';
 import ContentDecimal from './content/decimal.md';
 import ContentHexadecimal from './content/hexadecimal.md';
 
-const InfoBox = ({ system }: InfoBoxProps) => {
+const InfoBox = ({ system }: InfoBoxProps): JSX.Element => {
   const [content, setContent] = useState('');
 
+  const options = {
+    2: [ContentBinary, 'Binary_number'],
+    8: [ContentOctal, 'Octal'],
+    10: [ContentDecimal, 'Decimal'],
+    16: [ContentHexadecimal, 'Hexadecimal'],
+  };
+
   useEffect(() => {
-    const options = {
-      2: ContentBinary,
-      8: ContentOctal,
-      10: ContentDecimal,
-      16: ContentHexadecimal,
-    };
-    fetch(options[system])
+    fetch(options[system][0])
       .then(content => content.text())
       .then(text => setContent(text))
       .catch(error => console.error(error));
   }, [system]);
 
   return (
-    <Paper variant='outlined' sx={{ 'padding': '0px 15px', 'width': '1', }}>
-      <ReactMarkdown>
-        {content}
-      </ReactMarkdown>
+    <Paper variant='outlined' sx={{ 'padding': '0px 15px 15px 15px', 'width': '1', }}>
+      <Content content={content} />
+      <ReadMoreLink system={options[system][1]} />
     </Paper>
   );
 };
+
+const Content = ({ content }: { content: string }): JSX.Element => (
+  <ReactMarkdown>
+    {content}
+  </ReactMarkdown>
+);
+
+const ReadMoreLink = ({ system }: { system: string }): JSX.Element => (
+  <Link
+    color='primary'
+    href={`https://en.wikipedia.org/wiki/${system}`}
+    target='_blank'
+    underline='hover'
+    variant='subtitle1'
+  >
+    Read more
+  </Link>
+);
 
 export default InfoBox;
