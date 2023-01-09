ee43f8aeb011bc2b0e6923005e7ca09ce93d261f
Riku Rauhala <riku@rauhala.io>
2023-01-09 13:50:47 +0200

    Add JSDoc to convert.ts

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,5 +1,18 @@
 import { NumeralSystem } from '../types/types';
 
+/**
+ * Converts a number from base X to base Y.
+ *
+ * Supported systems:
+ * - Base 2 (binary)
+ * - Base 8 (octal)
+ * - Base 10 (decimal)
+ * - Base 16 (hexadecimal)
+ * @param {string} number The number to be converted.
+ * @param {NumeralSystem} input Input numeral system, base X.
+ * @param {NumeralSystem} output Output numeral system, base Y.
+ * @return {string} The converted number.
+ */
 const convert = (number: string, input: NumeralSystem, output: NumeralSystem): string => {
   return parseInt(number, input).toString(output).toUpperCase();
 };
