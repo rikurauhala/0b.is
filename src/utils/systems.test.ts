dc771bfce652c0373f382c3a66168f7ee94dafd6
Riku Rauhala <riku@rauhala.io>
2023-01-12 20:28:57 +0200

    Fix unit tests

--- src/utils/systems.test.ts
+++ src/utils/systems.test.ts
@@ -13,12 +13,12 @@ describe('systems', () => {
 
   test('key can be retrieved by value', () => {
     const binary = getNameByValue(2, Language.English);
-    expect(binary).toBe('Binary');
+    expect(binary).toBe('2 (binary)');
     const octal = getNameByValue(8, Language.English);
-    expect(octal).toBe('Octal');
+    expect(octal).toBe('8 (octal)');
     const decimal = getNameByValue(10, Language.English);
-    expect(decimal).toBe('Decimal');
+    expect(decimal).toBe('10 (decimal)');
     const hexadecimal = getNameByValue(16, Language.English);
-    expect(hexadecimal).toBe('Hexadecimal');
+    expect(hexadecimal).toBe('16 (hexadecimal)');
   });
 });
