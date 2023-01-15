84d859271a23a13b30adbb654b9a75b7b311d869
Riku Rauhala <riku@rauhala.io>
2023-01-15 20:42:35 +0200

    Update the footer version button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = ({ language }: FooterProps): JSX.Element => {
         />
         <FooterButton
           icon={<InfoIcon />}
-          text={`${languages['Version'][language]} 1.2.1`}
+          text={`${languages['Version'][language]} 1.2.2`}
           url={urlChangelog}
         />
       </Grid>
