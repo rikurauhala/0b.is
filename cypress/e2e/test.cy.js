d6b76c3000f6b62d8763e0b9507b7ce033a5de7e
Riku Rauhala <riku@rauhala.io>
2023-03-09 12:29:48 +0200

    Fix e2e tests

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,6 +3,6 @@ const url = 'http://localhost:3000';
 describe('0b.is', function() {
   it('Front page loads correctly', function() {
     cy.visit(url);
-    cy.contains('non-existent string')
+    cy.contains('0b.is')
   })
 })
